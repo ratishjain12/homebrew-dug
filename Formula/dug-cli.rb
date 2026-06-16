64de162d9464ec984e0bc5714ff6e50c9bc71695
Ratish jain <43003421+ratishjain12@users.noreply.github.com>
2026-06-16 12:54:11 +0530

    update: formula

--- Formula/dug-cli.rb
+++ Formula/dug-cli.rb
@@ -3,15 +3,9 @@ class DugCli < Formula
   homepage "https://github.com/ratishjain12/dug"
   version "0.1.0"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/ratishjain12/dug/releases/download/v#{version}/dug-macos-arm64"
-      sha256 "REPLACE_WITH_ARM64_SHA256"
-    end
-    on_intel do
-      url "https://github.com/ratishjain12/dug/releases/download/v#{version}/dug-macos-amd64"
-      sha256 "REPLACE_WITH_AMD64_SHA256"
-    end
+  on_arm do
+    url "https://github.com/ratishjain12/dug/releases/download/v#{version}/dug-macos-arm64"
+    sha256 "1c4fd5a4a542974e4b5823432d45e142d19566006ee7f7228c10a08b7d28a208"
   end
 
   def install
